2723e7128c156a49c3ed48601f81200093e21165
Utkarsh Kadu <145374112+Utkarsh8867@users.noreply.github.com>
2025-02-05 11:50:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//   },
+// });
+
+
 export default defineConfig({
-  plugins: [react()],
+   plugins: [react()],
   server: {
-    port: 5173,
+    host: '0.0.0.0',  // Exposes the server
+    port: 5173,       // Change if needed
   },
 });
+
